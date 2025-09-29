60f0ca5324e965ce84a424ee4b578f5e1b3fb04c
EmAnComChuaa <hihihahahohoaaa@gmail.com>
2025-09-29 22:55:14 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appication;
+
+import view.menu;
+
+/**
+ *
+ * @author Admin
+ */
+public class main {
+    public static void main(String[] args) {
+        menu display = new menu();
+        display.displayMenu();
+    }
+}
